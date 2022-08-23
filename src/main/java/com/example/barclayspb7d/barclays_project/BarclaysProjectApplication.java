66190501e54f23bc01c96a6ea8998f457a052d6a
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-08-23 11:23:05 +0530

    add template code

---
+++ src/main/java/com/example/barclayspb7d/barclays_project/BarclaysProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.barclayspb7d.barclays_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarclaysProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarclaysProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/barclayspb7d/barclays_project/BarclaysProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barclayspb7d.barclays_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarclaysProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
