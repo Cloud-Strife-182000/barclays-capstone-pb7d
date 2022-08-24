c15db654fe7112372f02318bdc838bb3236103d7
sourabhb77 <bujawadesourabhr@gmail.com>
2022-08-24 10:07:54 +0530

    Modified loan class

--- src/main/java/com/example/barclayspb7d/barclays_project/entities/LoanAccount.java
+++ src/main/java/com/example/barclayspb7d/barclays_project/entities/LoanAccount.java
@@ -6,36 +6,103 @@ import javax.persistence.*;
 @Entity
 @Table(name = "LoanAccount")
 public class LoanAccount {
-    // Loan Account ID
-    // Savings Account
-    // Total Loan Amount
-    // Interest Rate
-    // Tenure
-    // Status ( Approved / Ongoing / Closed).
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(unique = true, nullable = false)
-    private Long loanAccID;
+	@Column(name = "loan_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "loan_gen")
+	@SequenceGenerator(name = "loan_gen", sequenceName = "loan_id_seq")
+	private Long loanId;
 
-    @Column(nullable = false, unique = false, length = 12)
-    private String savingAccId;
+	@Column(name = "max_loan_grant", nullable = false, unique = false)
+	private Double maxLoanGrant;
 
-    @Column(nullable = false, unique = false)
-    private Long totalLoanAount;
+	@Column(name = "interest_rate", nullable = false, unique = false)
+	private Double interestRate = 7.0;
 
-    @Column(nullable = false, unique = false)
-    private Integer interestRate;
+	@Column(name = "tenure", nullable = false, unique = false)
+	private Integer tenure;
 
-    @Column(nullable = false, unique = false)
-    private Integer tenure;
+	@Column(name = "loan_amount", nullable = false, unique = false)
+	private Long loanAmount;
 
-    enum StatusEnum {
-        APPROVED,
-        ONGOING,
-        CLOSED
-    }
+    @Column(name = "loan_status", nullable = false, unique = false)
+	private String loanStatus;
 
-    @Column(nullable = false, unique = false)
-    private StatusEnum status;
+	@OneToOne(targetEntity = User.class)
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public LoanAccount() {
+
+	}
+
+	public LoanAccount(Double maxLoanGrant, Double interestRate, Integer tenure, Long loanAmount, String loanStatus) {
+		super();
+		this.maxLoanGrant = maxLoanGrant;
+		this.interestRate = 7.0;
+		this.tenure = tenure;
+		this.loanAmount = loanAmount;
+        this.loanStatus = "PPENDING";
+	}
+
+	@Override
+	public String toString() {
+		return "Loan [loanId=" + loanId + ", maxLoanGrant=" + maxLoanGrant + ", interestRate=" + interestRate
+				+ ", tenure=" + tenure + ", loanAmount=" + loanAmount + "]";
+	}
+
+	public Long getLoanId() {
+		return loanId;
+	}
+
+	public Double getMaxLoanGrant() {
+		return maxLoanGrant;
+	}
+
+	public void setMaxLoanGrant(Double maxLoanGrant) {
+		this.maxLoanGrant = maxLoanGrant;
+	}
+
+	public Integer getTenure() {
+		return tenure;
+	}
+
+	public void setTenure(Integer tenure) {
+		this.tenure = tenure;
+	}
+
+	public Long getLoanAmount() {
+		return loanAmount;
+	}
+
+	public void setLoanAmount(Long loanAmount) {
+		this.loanAmount = loanAmount;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+    public String getLoanStatus() {
+		return loanStatus;
+	}
+
+	public void setTenure(String loanStatus) {
+		this.loanStatus = loanStatus;
+	}
+
+    // enum StatusEnum {
+    //     APPROVED,
+    //     ONGOING,
+    //     CLOSED
+    // }
+
+    // @Column(nullable = false, unique = false)
+    // private StatusEnum status;
 }
