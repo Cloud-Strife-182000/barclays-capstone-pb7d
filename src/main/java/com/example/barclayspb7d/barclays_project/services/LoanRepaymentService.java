37406940c5fe5986d5f3a8f4357593d9f316560f
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-08-24 14:59:32 +0530

    Update LoanRepaymentService.java

--- src/main/java/com/example/barclayspb7d/barclays_project/services/LoanRepaymentService.java
+++ src/main/java/com/example/barclayspb7d/barclays_project/services/LoanRepaymentService.java
@@ -1,9 +1,16 @@
+import javax.transaction.Transactional;
+
+import com.example.barclayspb7d.barclays_project.dao.RepaymentRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 @Service
 @Transactional
 public class LoanRepaymentService {
 
 	@Autowired
-	LoanRepaymentRepository LoanRepaymentRepository;
+	RepaymentRepository LoanRepaymentRepository;
 	
 	
 	public double CalcEmi() {
