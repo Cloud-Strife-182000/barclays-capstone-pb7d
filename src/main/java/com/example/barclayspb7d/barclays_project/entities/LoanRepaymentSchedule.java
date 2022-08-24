3310d6c33805f396e90cd3f8e5f69a9f13bcd8bf
Srikanth Govindaraghavan <sri.gr81@gmail.com>
2022-08-24 13:36:30 +0530

    Update LoanRepaymentSchedule.java

--- src/main/java/com/example/barclayspb7d/barclays_project/entities/LoanRepaymentSchedule.java
+++ src/main/java/com/example/barclayspb7d/barclays_project/entities/LoanRepaymentSchedule.java
@@ -25,6 +25,62 @@ public class LoanRepaymentSchedule {
 	private String Status;
   @Column(name = "Months", nullable = false, unique = false)
 	private Double Months;
-  
+
+	public LoanAccount getLoanAccount() {
+		return this.loanAccount;
+	}
+
+	public void setLoanAccount(LoanAccount loanAccount) {
+		this.loanAccount = loanAccount;
+	}
+
+	public Double getEMI() {
+		return this.EMI;
+	}
+
+	public void setEMI(Double EMI) {
+		this.EMI = EMI;
+	}
+
+	public Double getPrincipalAmount() {
+		return this.PrincipalAmount;
+	}
+
+	public void setPrincipalAmount(Double PrincipalAmount) {
+		this.PrincipalAmount = PrincipalAmount;
+	}
+
+	public Double getOutstanding() {
+		return this.Outstanding;
+	}
+
+	public void setOutstanding(Double Outstanding) {
+		this.Outstanding = Outstanding;
+	}
+
+	public Double getIntrestAmount() {
+		return this.IntrestAmount;
+	}
+
+	public void setIntrestAmount(Double IntrestAmount) {
+		this.IntrestAmount = IntrestAmount;
+	}
+
+	public String getStatus() {
+		return this.Status;
+	}
+
+	public void setStatus(String Status) {
+		this.Status = Status;
+	}
+
+	public Double getMonths() {
+		return this.Months;
+	}
+
+	public void setMonths(Double Months) {
+		this.Months = Months;
+	}
+
 
 }
